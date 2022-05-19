827fb999f15726a83ed614d4ea57884b4b1e1b44
Dan Gurney <gurney.dan@gmail.com>
2022-05-19 11:42:29 +0100

    Reach parity with SvelteKit project

--- app/root.tsx
+++ app/root.tsx
@@ -7,12 +7,17 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from '@remix-run/react';
+import styles from './styles/app.css';
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
 	title: 'Dan Gurney - Personal Site',
 	viewport: 'width=device-width,initial-scale=1',
 });
+
+export function links() {
+	return [{ rel: 'stylesheet', href: styles }];
+}
 
 export default function App() {
 	return (
@@ -22,6 +27,14 @@ export default function App() {
 				<Links />
 			</head>
 			<body>
+				<nav className="flex flex-row justify-start px-4 py-2 space-x-4">
+					<a href="/">Dan Gurney</a>
+					<span>/</span>
+					<a href="/software">Software</a>
+					<a href="/music">Music</a>
+					<a href="/blog">Blog</a>
+				</nav>
+
 				<Outlet />
 				<ScrollRestoration />
 				<Scripts />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,9 @@
 export default function Index() {
-	return <div>Hello world</div>;
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<div className="flex flex-row items-center justify-center mt-24">
+				<h1>Hi, I'm Dan</h1>
+			</div>
+		</div>
+	);
 }
